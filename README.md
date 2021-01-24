047fbe5331b50c36f2858276338a2c0a5d0d8b35
dexterstack99 <innocentd308@gmail.com>
2021-01-24 17:43:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-j9vrnm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-j9vrnm)
---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+    <div className="container">
+    <div>
+    <img className="profile" src="https://i.pinimg.com/564x/ae/d7/b9/aed7b976667d69314e3a5c05287fa2f6.jpg" alt="Roger Abel Monsengo"></img>
+    </div>
+    <div>
+    <h1 className="username">Roger Abel Monsengo</h1>
+    <h5 className="refname">"Douglas"</h5>
+    </div>
+    </div>
+      <h1 className="sec-title">About Roger Abel Monsengo</h1>
+      <div className="details">
+      <div className="titles">
+      <h4>Forename(s)</h4>
+      <h4>Surname</h4>
+      <h4>Date of birth</h4>
+      <h4>Gender</h4>
+      <h4>Marital Status</h4>
+      <h4>Nationality</h4>
+      <h4>City Of Birth</h4>
+      <h4>Country of growth</h4>
+
+      <h4>Languages</h4>
+      <h4>Health</h4>
+      <h4>Criminal Record</h4>
+      </div>
+      <div className="values">
+      <h4>Roger Abel</h4>
+      <h4>Monsengo</h4>
+      <h4>4<sup>th</sup> July 1971</h4>
+      <h4>Male</h4>
+      <h4>Single Father</h4>
+      <h4>Congolese</h4>
+      <h4>Kinshasa</h4>
+      <h4>France  ~   Paris| 1989 ~ 1995</h4>
+      <h4>English & French</h4>
+      <h4>Good</h4>
+      <h4>None</h4>
+      </div>
+      </div>
+
+    
+      <h1 className="sec-title">Academic Background</h1>
+      <div className="details">
+      <div className="titles">
+      <h4>Last School Attended</h4>
+      <h4>Place</h4>
+      <h4>Science Art</h4>
+      <h4>Place</h4>
+      <h4>Military Academic & Service</h4>
+      <h4>Place</h4>
+      <h4>Science Spiritual</h4>
+      <h4>Place</h4>
+      
+      </div>
+      <div className="values">
+      <h4>I.T.I Kitomesa (Technical)</h4>
+      <h4>Kinshasa/Congo D.R.C</h4>
+      <h4>Taekwondo Martial Arts</h4>
+      <h4>Paris   ~   France| 1990 - 1995</h4>
+      <h4>Kamina Military Base</h4>
+      <h4>Kamina  |Congo D.R.C |1998 - 2004</h4>
+      <h4>Institut of science of the soul</h4>
+      <h4>Wynberg - Cape Town (2007 - 2017)</h4>
+      </div>
+      
+      </div>
+
+      <h1 className="sec-title">Work Experience</h1>
+      <div className="details">
+      <div className="titles">
+      <h4>Company Name</h4>
+      <h4>Position</h4>
+      <h4>Duration</h4>
+      <h4>Reference</h4>
+      
+      
+      </div>
+      <div className="values">
+      <h4>Good Food (Technical)</h4>
+      <h4>Private Security Guard</h4>
+      <h4>2 years</h4>
+      <h4>Angie Smith ~ +27 60 606 6014</h4>
+      
+      </div>
+      
+      </div>
+
+    </div>
+  );
+}
